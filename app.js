8c3ac52abe6320498ac027adaef385893191626c
Marco Abraham Bonissi <marco@MacBook-Air-di-Marco.local>
2022-03-22 15:37:29 +0100

    Added HTTPS support

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
 
 const app = express()
-const port = 3030
+const port = 443
 const bodyParser = require('body-parser')
 const oauthServer = require('./oauth/server.js')
 
 const DebugControl = require('./utilities/debug.js')
 
+const http = require('http')
+const https = require('https')
+
+const fs = require('fs')
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(DebugControl.log.request())
+
+//Redirect all http requests to https
+app.all('*', function(req, res, next){
+  if (req.secure) return next()
+  res.redirect('https://' + req.hostname + req.url)
+})
 
 //app.use('/client', require('./routes/client.js')) // Client routes
 app.use('/oauth', require('./routes/auth.js')) // routes to access the auth stuff
@@ -21,8 +32,11 @@ app.use('/secure', (req,res,next) => {
 },oauthServer.authenticate(), require('./routes/secure.js')) // routes to access the protected stuff
 //app.use('/', (req,res) => res.redirect('/client'))
 
-
-app.listen(port)
+http.createServer(app).listen(80)
+https.createServer({
+  key: fs.readFileSync('./cert/server.key'),
+  cert: fs.readFileSync('./cert/server.cert'),
+}, app).listen(port)
 console.log("Oauth Server listening on port ", port)
 
 module.exports = app // For testing
